0d15db3346e3ef09df4994899b4d2a865052c949
root <root@anacreon.damiens.info>
2015-11-11 13:25:17 +0100

    clone, volumes, env osm_pbf

---
+++ clone.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/ndamiens/docker-geopic-mapnik osm-mapnik
+git clone https://github.com/ndamiens/docker-geopic-postgres-osm osm-db
+git clone https://github.com/ndamiens/docker-mapproxy-cache osm-mapproxy
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BASE=/srv/geopicardie
-mkdir -p $BASE/volumes/postgres $BASE/volumes/mongo
+mkdir -p $BASE/volumes/postgres $BASE/volumes/mongo $BASE/volumes/osm-data
 chmod a+w $BASE/volumes/postgres
 chmod a+w $BASE/volumes/mongo
+chmod a+w $BASE/volumes/osm-data
