b2dd9637df5a6a8ed37516acc725818821f29058
Nicolas Damiens <nicolas@damiens.info>
2015-11-19 07:59:53 +0100

    expose mongodb, nomaster

---
+++ osm-mongodb/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -m
+
+mongodb_cmd="mongod"
+cmd="$mongodb_cmd --httpinterface --rest"
+if [ "$AUTH" == "yes" ]; then
+    cmd="$cmd --auth"
+fi
+
+if [ "$JOURNALING" == "no" ]; then
+    cmd="$cmd --nojournal"
+fi
+
+if [ "$OPLOG_SIZE" != "" ]; then
+    cmd="$cmd --oplogSize $OPLOG_SIZE"
+fi
+
+if [ "$REPL_SET" != "" ]; then
+    cmd="$cmd --replSet $REPL_SET"
+fi
+
+$cmd &
+
+if [ ! -f /data/db/.mongodb_password_set ]; then
+    /set_mongodb_password.sh
+fi
+
+fg
+
+rm /data/mongod.lock
