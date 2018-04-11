f534ae0a3d325962938658463953c6a113bacb28
Sébastien DA ROCHA <sebastien@da-rocha.net>
2018-04-11 11:35:41 +0200

    replace submodules by a script

---
+++ get_dependencies.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/geopicardie/docker-geopic-mapnik osm-mapnik
+git clone https://github.com/geopicardie/docker-geopic-postgres-osm osm-db -b update_osm
+git clone https://github.com/geopicardie/docker-mapproxy-cache osm-mapproxy
+
