acaaffc0143274da3f169f3db812324e04d81aba
Sébastien DA ROCHA <sebastien@da-rocha.net>
2018-04-11 16:56:53 +0200

    Met à jour les depences et pointe mapproxy sur la branche osm-update

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,4 +1,36 @@
-git clone https://github.com/geopicardie/docker-geopic-mapnik osm-mapnik
-git clone https://github.com/geopicardie/docker-geopic-postgres-osm osm-db -b update_osm
-git clone https://github.com/geopicardie/docker-mapproxy-cache osm-mapproxy
+#! /bin/bash
+
+BRANCH_MAPNIK=master
+BRANCH_DB=update_osm
+BRANCH_MAPPROXY=osm-update
+
+echo update osm-mapnik
+if [ -d osm-mapnik/ ]; then
+    cd osm-mapnik
+    git pull
+    git checkout $BRANCH_MAPNIK
+    cd ..
+else
+    git clone https://github.com/geopicardie/docker-geopic-mapnik osm-mapnik -b $BRANCH_MAPNIK
+fi
+
+echo update osm-db
+if [ -d osm-db/ ]; then
+    cd osm-db
+    git pull
+    git checkout $BRANCH_DB
+    cd ..
+else
+    git clone https://github.com/geopicardie/docker-geopic-postgres-osm osm-db -b $BRANCH_DB
+fi
+
+echo update osm-mapproxy
+if [ -d osm-mapproxy/ ]; then
+    cd osm-mapproxy
+    git pull
+    git checkout $BRANCH_MAPPROXY
+    cd ..
+else
+    git clone https://github.com/geopicardie/docker-mapproxy-cache osm-mapproxy -b $BRANCH_MAPPROX
+fi
 
