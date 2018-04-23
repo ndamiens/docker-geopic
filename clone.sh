ed9ecc0c1511c496f1067f61ed59b4d701d643b0
Sébastien DA ROCHA <sebastien@da-rocha.net>
2018-04-23 11:47:20 +0200

    Suppression vieux script de dependences

--- clone.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/geopicardie/docker-geopic-mapnik osm-mapnik
-git clone https://github.com/geopicardie/docker-geopic-postgres-osm osm-db
-git clone https://github.com/geopicardie/docker-mapproxy-cache osm-mapproxy
